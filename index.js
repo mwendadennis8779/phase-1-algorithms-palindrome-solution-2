18ae86452fc6fa9cd925600370f1034f0441b04e
Dennis <mwendadennis8779@gmail.com>
2022-12-08 20:02:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // Write your algorithm here
+ for(let a =0 ; a < word.length/2 ; a++){
+  const z = word.length -1 - a;
+  
+
+  if (word[a] !== word[z]) 
+  return false
+ }
+
+ return true
 }
 
 /* 
   Add your pseudocode here
+
+  Add your pseudocode here
+  Write code that will check if the index position of a letter on both ends of a word coming inwards and xheck if they will match
+  iterete from the beginning to the middle of the word.
+  describe a variable to represent the letters
+  use a if statement which returns false when the letters do not match
 */
+// 
+// 
+ 
+
 
 /*
   Add written explanation of your solution here
